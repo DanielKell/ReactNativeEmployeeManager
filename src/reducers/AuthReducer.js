d54bfbaa076e9ffea131dcb95ac2bfbddebea5e3
DanielKell <danielkell.ubc@gmail.com>
2017-11-14 20:49:12 -0800

    Hooked up password field to redux

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,7 +1,11 @@
 //Handle all the authentication logic
-import { EMAIL_CHANGED } from '../actions/types';
+import { EMAIL_CHANGED, PASSWORD_CHANGED } from '../actions/types';
 
-const INITIAL_STATE = {email: ''};
+const INITIAL_STATE = {
+    email: '',
+    password: ''
+};
+//Define empty string to inform others what this reducer is responsible for
 
 export default (state = INITIAL_STATE , action) => {
     switch (action.type) {
@@ -9,6 +13,8 @@ export default (state = INITIAL_STATE , action) => {
         case EMAIL_CHANGED: 
             return { ...state, email: action.payload }; 
             //Make new object with all the old state, and include email data
+        case PASSWORD_CHANGED:
+            return { ...state, password: action.payload};
 
         default:
             return state;
